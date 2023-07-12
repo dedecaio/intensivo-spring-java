70016f35763823f4185fc6f7a3007365cdd16ac2
Caio <caio.florees2005@gmail.com>
2023-07-11 21:30:20 -0300

    Making the other 2 tables

--- dslist/src/main/java/com/caio/dslist/controllers/GameController.java
+++ dslist/src/main/java/com/caio/dslist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.caio.dslist.controllers;
 
+import com.caio.dslist.dto.GameDTO;
 import com.caio.dslist.dto.GameMinDTO;
 import com.caio.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,10 @@ public class GameController {
     @GetMapping("")
     public List<GameMinDTO> listGames(){
         return service.listAllGames();
+    }
+
+    @GetMapping("{id}")
+    public GameDTO listOneGameById(@PathVariable Long id){
+        return service.findById(id);
     }
 }
--- dslist/src/main/java/com/caio/dslist/services/GameService.java
+++ dslist/src/main/java/com/caio/dslist/services/GameService.java
@@ -1,13 +1,16 @@
 package com.caio.dslist.services;
 
+import com.caio.dslist.dto.GameDTO;
 import com.caio.dslist.dto.GameMinDTO;
 import com.caio.dslist.entities.Game;
 import com.caio.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameService {
@@ -18,6 +21,7 @@ public class GameService {
         this.repository = repository;
     }
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> listAllGames(){
         List<Game> games = repository.findAll();
         List<GameMinDTO> gameMinDTOList = new ArrayList<>();
@@ -26,6 +30,18 @@ public class GameService {
             gameMinDTOList.add(gameMinDTO);
         }
         return gameMinDTOList;
+
     }
 
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Optional<Game> game = repository.findById(id);
+        if(game == null){
+            throw new RuntimeException("Game not found - "+ id);
+        }else {
+            return new GameDTO(game.get());
+        }
+
+    }
 }
